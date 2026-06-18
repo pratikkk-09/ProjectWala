99243b69adbd72332b392e926be8536b18884876
pratikkk-09 <nasscom6021@gmail.com>
2026-06-18 19:41:46 +0530

    Delete ProjectWala directory

--- ProjectWala/test-verify.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-try {
-  initializeApp({ projectId: 'gen-lang-client-0266183284' });
-  const auth = getAuth();
-  console.log('Firebase admin auth initialized');
-} catch (e) {
-  console.log('failed', e);
-}
